8e54bfe615714b1f10cab711f178a92800cd86d7
thuyanhnguyen168 <thuyanhnguyen168@gmail.com>
2016-09-14 11:41:21 +0700

    Update redirectcopy.js

--- redirectcopy.js
+++ redirectcopy.js
@@ -2,7 +2,7 @@ function FSAddOriginalLink(){
     var body_element = document.getElementsByTagName('body')[0];
     var selection;
     selection = window.getSelection();
-    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://bit.ly/2cUtzDe'>http://faceseo.vn"+document.location.pathname+"</a>";
+    var pagelink = "<br/><br/>MUON COPY THI VAO DAY : <a href='http://bit.ly/2cUtzDe'>http://bit.ly/2cUtzDe"+document.location.pathname+"</a>";
     var copytext = pagelink;
     var newdiv = document.createElement('div');
     newdiv.style.position='absolute';
